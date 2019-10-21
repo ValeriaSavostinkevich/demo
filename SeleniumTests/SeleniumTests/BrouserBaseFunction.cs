660e1493f4d166e92ecf4f19df961fe0fffc7b56
Valeria Savostinkevich <valeriasav9@gmail.com>
2019-10-21 23:00:10 +0300

    Add SeleniumTests

---
+++ SeleniumTests/SeleniumTests/BrouserBaseFunction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumTests
+{
+    public abstract class BrouserBaseFunction
+    {
+        public IWebDriver webDriver;
+
+        [SetUp]
+        public void OpenBrouserAndGoToTheTestSite()
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(200);
+            webDriver.Navigate().GoToUrl("https://www.americanairlines.com.ru/");
+        }
+
+        [TearDown]
+        public void CloseBrouser()
+        {
+            webDriver.Quit();
+        }
+
+        protected IWebElement GetWebElementById(string Id)
+        {
+            return webDriver.FindElement(By.Id(Id));
+        }
+
+        protected IWebElement GetWebElementByXPath(string xPath)
+        {
+            return webDriver.FindElement(By.XPath(xPath));
+        }
+    }
+}
---
+++ SeleniumTests/SeleniumTests/Tests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Tests : BrouserBaseFunction
+    {
+        private const string ErrorTextForEmptyString =
+            "Укажите город возвращения и повторите попытку.";
+
+        private const string ErrorTextForTheWrongReservationNumber =
+            "Введите действительный шестизначный номер бронирования.";
+
+        //Пустое поле "В" при поиске рейса
+        //Шаги:
+        //-Зайти на сайт https://www.americanairlines.com.ru/intl/ru/
+        //-Указать в поле "из" "MSQ"
+        //-Оставить поле "в" пустым
+        //-нажимаем кнопку "Поиск"
+        //Ожидаемый результат: Появление сообщения "Укажите город отправления и повторите попытку".
+        [Test]
+        public void SearchWithoutEnteringTheCityOfArrival()
+        {
+            #region TestData
+
+            const string departureCityText = "MSQ";
+
+            #endregion
+
+            var departureCity = GetWebElementById("reservationFlightSearchForm.originAirport");
+            departureCity.SendKeys(departureCityText);
+            var searchButton = GetWebElementById("bookingModule-submit");
+            searchButton.Click();
+            var errorMessage = GetWebElementByXPath("//li[@class = 'errorMessage']");
+            string error = errorMessage.Text;
+            Assert.AreEqual(ErrorTextForEmptyString, error);
+        }
+
+        //Ввод несуществующего номера резервации при просмотре вкладки "Мои перелёты"
+        //Шаги:
+        //-Зайти на сайт https://www.americanairlines.com.ru/intl/ru/
+        //-Перейти на вкладку "Мои перелёты"
+        //-Указать в поле "Имя пассажира"  Jane
+        //-Указать в поле "Фамилия пассажира"  Fox
+        //-Указать в поле "Номер рейса" 123456
+        //-нажимаем кнопку "Найти бронирование"
+        //Ожидаемый результат: Появление сообщения "Введите действительный шестизначный номер бронирования.".
+        [Test]
+        public void SearchWithWrongReservationNumber()
+        {
+
+            #region TestData
+
+            const string firstNameText = "Jane";
+            const string lastNameText = "Fox";
+            const string reservationNumberText = "123456";
+
+            #endregion
+
+            var myTripsCheckIn = GetWebElementById("jq-myTripsCheckIn");
+            myTripsCheckIn.Click();
+
+            var firstName = GetWebElementById("retr-firstName");
+            firstName.SendKeys(firstNameText);
+
+            var lastName = GetWebElementById("retr-lastName");
+            lastName.SendKeys(lastNameText);
+
+            var reservationNumber = GetWebElementById("retr-recordLocator");
+            reservationNumber.SendKeys(reservationNumberText);
+
+            var searchButton = GetWebElementById("prs-submit");
+            searchButton.Click();
+
+            var errorMessage = GetWebElementByXPath("//li[@class = 'errorMessage']");
+            string error = errorMessage.Text;
+
+            Assert.AreEqual(ErrorTextForTheWrongReservationNumber, error);
+        }
+    }
+}
